612b024a729439e6774a1356f0689fbb986ef6c1
djfox12rus <djfox12rus@gmail.com>
2017-08-17 20:39:34 +0300

    Сборщик мусора с пулом и умными указателями. v 0.000003

--- SafeMemory/SafeMemory.cpp
+++ SafeMemory/SafeMemory.cpp
@@ -3,13 +3,41 @@
 
 #include "stdafx.h"
 #include "../lib/MemWiz.h"
-//extern MemoryControl::_memory_interface mem_wiz;
+
+extern MemoryControl::_memory_interface mem_wiz;
 
 int main()
 {
-	
-	MemoryControl::test();
-	
+	ldouble_sptr d_arr = ldouble_sptr(double(), 50);
+	std::cout << "d_arr: ";
+	for (int i = 0; i < 50; i++) {
+		d_arr[i] = pow((double)i / 50, i);
+		std::cout << d_arr[i] << ", ";
+	}
+	std::cout << "\n";
+	ldouble_sptr d_arr1 = ldouble_sptr(double(), 50);
+	std::cout << "d_arr1: ";
+	for (int i = 0; i < 50; i++) {
+		d_arr1[i] = i*d_arr[i];
+		std::cout << d_arr1[i] << ", ";
+	}
+	std::cout << "\n";
+	ldouble_sptr d_arr2;
+	d_arr2 = d_arr1;
+	std::cout << "d_arr2: ";
+	for (int i = 0; i < 50; i++) {
+		std::cout << d_arr2[i] << ", ";
+	}
+	std::cout << "\n";
+
+
+	d_arr = d_arr2;
+	std::cout << "d_arr: ";
+	for (int i = 0; i < 50; i++) {
+		std::cout << d_arr[i] << ", ";
+	}
+	std::cout << "\n";
+
 
 	MemoryControl::mem_wiz.~_memory_interface();
     return 0;
--- lib/MemWiz.h
+++ lib/MemWiz.h
@@ -12,85 +12,94 @@ namespace MemoryControl {
 	
 	class _memory_interface {
 	private:
+		template<class T>
 		friend class _smart_ptr;
 		struct ref {
 			uint32_t ref_count;
 			void * mem_ref;
 		};
 		
-		ref *reference;	//пока таблица адресации блока пам€ти находитс€ непосредственно в блоке пам€ти. ќднако поскольку внешние "умные" указатели на сам пул пам€ти никогда напр€мую не ссылаютс€,
-						//а ссылаютс€ на нужную запись в таблице, имеет смысл под адреса выдел€ть пам€ть отдельно. “огда дл€ основного пула пам€ти можно будет использовать сишный realloc 
-						//или calloc + memmove (занул€€ новый кусок пам€ти), а ссылки востанавливать с помощью арифметики указателей. “аким образом возможно сам пул сделать полностью динамическим.
-		void *mem_pool;
-		void *block;
+		ref *reference_table;	//таблица адресации хранитс€ в виде массива. ¬озможно сделать еЄ в виде списка std::list, чтобы не заботитс€ о выходе за пределы адресного пол€	
+		void *block;		
+		void *block_end;
 
 		ref* allocate_mem(size_t _size_of_type, size_t _size_of_arr = 1);
 
 	public:
-		
-		
-
-		template<class T>
-		class _smart_ptr {
-		private:
-			ref *s_ref;
-			bool check_if_can_continue(size_t);
-		public:
-			_smart_ptr() : s_ref(nullptr){}
-			_smart_ptr(T _obj);
-			_smart_ptr(T _obj, size_t _size_of_array);//так же объ€вление массива. возможно имеет смысл сделать отдельный класс указателей только под массивы.
-			~_smart_ptr() {
-				std::cout << "dest smart\n";
-				std::cout << "memory adress: "<< s_ref;
-				if (s_ref) {
-					if (s_ref->ref_count > 0) {
-						s_ref->ref_count--;
-						std::cout << ", links left: " << s_ref->ref_count << "\n";
-					}
-					else {//возможен какой-либо кос€к с учЄтом всех ссылок.
-
-					}
-				}
-				else
-					std::cout << ", links' not initialized\n";
-				//s_ref = nullptr;
-			}
-
-			//Ќебезопасно! Ќельз€ удал€ть ("забывать") сам smart_pointer! Ќельз€ использовать delete!
-			T* get_ptr_unsafe();
-			
-			T at(size_t);
-
-			void alloc(T _obj, size_t _size_of_arr = 1);
-			void realloc(size_t);
-
-			T* operator->();
-			T& operator* ();
-
-			T& operator[](size_t);
-
-			_smart_ptr<T> operator=(_smart_ptr<T>& _left);
-			
-		};
-
 		_memory_interface();
 		~_memory_interface();
-
-		template<class T>
-		_smart_ptr get_smart(T _obj, size_t _size_of_array = 1);
 		
 	};
 	extern _memory_interface mem_wiz;
-	int test();		
+	int test();	
+
+
+
+	template<class T>
+	class _smart_ptr {
+	private:
+		_memory_interface::ref *s_ref;
+		bool check_if_can_continue(size_t);
+	public:
+		_smart_ptr();
+		_smart_ptr(T _obj);
+		_smart_ptr(T _obj, size_t _size_of_array);//так же объ€вление массива. возможно имеет смысл сделать отдельный класс указателей только под массивы.
+		~_smart_ptr() {
+			std::cout << "dest smart\n";
+			std::cout << "memory adress: " << s_ref;
+			if (s_ref) {
+				if (s_ref->ref_count > 0) {
+					s_ref->ref_count--;
+					std::cout << ", links left: " << s_ref->ref_count << "\n";
+				}
+				else {//возможен какой-либо кос€к с учЄтом всех ссылок.
+
+				}
+			}
+			else
+				std::cout << ", links' not initialized\n";
+			//s_ref = nullptr;
+		}
+
+		//Ќебезопасно! Ќельз€ удал€ть ("забывать") сам smart_pointer! Ќельз€ использовать delete!
+		T* get_ptr_unsafe();
+
+		T at(size_t);
+
+		void alloc(T _obj, size_t _size_of_arr = 1);
+		void realloc(size_t);
+
+		T* operator->();
+		T& operator* ();
+
+		T& operator[](size_t);
+
+		_smart_ptr<T>& operator=(_smart_ptr<T>& _left);
+
+		void get_smart(T &_obj);
+
+	};
+
+
 
 }
 
 //не придумал ничего умнее.
-typedef MemoryControl::_memory_interface::_smart_ptr<int8_t> sp_int8_t;
-typedef MemoryControl::_memory_interface::_smart_ptr<int16_t> sp_int16_t;
-typedef MemoryControl::_memory_interface::_smart_ptr<int32_t> sp_int32_t;
-typedef MemoryControl::_memory_interface::_smart_ptr<int64_t> sp_int64_t;
-typedef MemoryControl::_memory_interface::_smart_ptr<double> sp_double;
+//signed
+typedef MemoryControl::_smart_ptr<int8_t> int8__sptr;
+typedef MemoryControl::_smart_ptr<int16_t> int16__sptr;
+typedef MemoryControl::_smart_ptr<int32_t> int32__sptr;
+typedef MemoryControl::_smart_ptr<int64_t> int64_sptr;
+//unsigned
+typedef MemoryControl::_smart_ptr<uint8_t> uint8__sptr;
+typedef MemoryControl::_smart_ptr<uint16_t> uint16__sptr;
+typedef MemoryControl::_smart_ptr<uint32_t> uint32__sptr;
+typedef MemoryControl::_smart_ptr<uint64_t> uint64_sptr;
+//floating
+typedef MemoryControl::_smart_ptr<float> float_sptr;
+typedef MemoryControl::_smart_ptr<double> double_sptr;
+typedef MemoryControl::_smart_ptr<long double> ldouble_sptr;
 
+#define MY_DOUBLE MemoryControl::_smart_ptr<long double> 
 
 #endif //MEMWIZ_H
